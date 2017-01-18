29ec7d17e81ba7e0861bd6892e3c732c17d48e34
Arman Abbasi <arman.abbasi@marksandspencer.com>
2017-01-18 21:33:03 +0000

    Don't need these comments

--- src/client-entry.js
+++ src/client-entry.js
@@ -1,13 +1,10 @@
 import 'es6-promise/auto';
 import { app, store } from './app';
 
-// prime the store with server-initialized state.
-// the state is determined during SSR and inlined in the page markup.
 store.replaceState(window.__INITIAL_STATE__);
 
 app.$mount('#app');
 
-// service worker
 if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/service-worker.js')
+    navigator.serviceWorker.register('/service-worker.js');
 }
