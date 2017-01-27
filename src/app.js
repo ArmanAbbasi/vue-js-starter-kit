f67199dd06e3a7bc52a24846699e338a9c9d8690
Arman Abbasi <arman.abbasi@marksandspencer.com>
2017-01-27 14:58:47 +0000

    Cleanup of comments

--- src/app.js
+++ src/app.js
@@ -5,8 +5,6 @@ import router from './router';
 import { sync } from 'vuex-router-sync'
 import labels from './data/labels.json';
 
-// sync the router with the vuex store.
-// this registers `store.state.route`
 sync(store, router);
 
 Vue.mixin({
@@ -15,21 +13,10 @@ Vue.mixin({
     }
 });
 
-// register global utility filters.
-// Object.keys(filters).forEach(key => {
-//     Vue.filter(key, filters[key])
-// });
-
-// create the app instance.
-// here we inject the router and store to all child components,
-// making them available everywhere as `this.$router` and `this.$store`.
 const app = new Vue({
     router,
     store,
     ...App
 });
 
-// expose the app, the router and the store.
-// note we are not mounting the app here, since bootstrapping will be
-// different depending on whether we are in a browser or on the server.
 export { app, router, store }
