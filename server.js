da1c2d50b2937c004d9c192ecf7a15032e766e0c
Arman Abbasi <arman.abbasi@marksandspencer.com>
2017-01-31 15:19:51 +0000

    Some cleanup of server init

--- server.js
+++ server.js
@@ -1,33 +1,36 @@
 const fs = require('fs');
 const path = require('path');
 const express = require('express');
+const app = express();
 const compression = require('compression');
+const staticAsset = require('static-asset');
+const zLib = require('zlib');
 const serialize = require('serialize-javascript');
 const resolve = file => path.resolve(__dirname, file);
 
 const MAX_CACHE_SIZE = 1000;
 const MAX_CACHE_AGE_MS = 1000 * 60 * 15;
+
+const ONE_YEAR_IN_MILLIS = 31557600000;
+const APP_PORT_NUM = process.env.PORT || 3000;
+
 const TEMPLATE_APP_MARKET = '<app/>';
 const IS_PROD = process.env.NODE_ENV === 'production';
-const PORT = process.env.PORT || 8080;
+
 const DISTRIBUTION_FOLDER = 'dist';
 const BUILD_FOLDER = 'build';
-const SERVER_INFO =
-    `express/${require('express/package.json').version} ` +
-    `vue-server-renderer/${require('vue-server-renderer/package.json').version}`;
-const app = express();
 
 let generatedHtml;
 let renderer;
 
-const createRenderer = (bundle) => require('vue-server-renderer').createBundleRenderer(bundle, {
+const vueJsServerRenderer = (bundle) => require('vue-server-renderer').createBundleRenderer(bundle, {
     cache: require('lru-cache')({
         max: MAX_CACHE_SIZE,
         maxAge: MAX_CACHE_AGE_MS
     })
 });
 
-const parseIndex = (template) => {
+const findPlaceholderInTemplateAndReplace = (template) => {
     const APP_MARKER_IDX = template.indexOf(TEMPLATE_APP_MARKET);
     return {
         head: template.slice(0, APP_MARKER_IDX),
@@ -36,41 +39,42 @@ const parseIndex = (template) => {
 };
 
 if (IS_PROD) {
-    // in production: create server renderer and index HTML from real fs
-    renderer = createRenderer(fs.readFileSync(resolve(`./${DISTRIBUTION_FOLDER}/server-bundle.js`), 'utf-8'));
-    generatedHtml = parseIndex(fs.readFileSync(resolve(`./${DISTRIBUTION_FOLDER}/index.html`), 'utf-8'));
+    renderer = vueJsServerRenderer(fs.readFileSync(resolve(`./${DISTRIBUTION_FOLDER}/server-bundle.js`), 'utf-8'));
+    generatedHtml = findPlaceholderInTemplateAndReplace(fs.readFileSync(resolve(`./${DISTRIBUTION_FOLDER}/index.html`), 'utf-8'));
 } else {
-    // in development: setup the dev server with watch and hot-reload,
-    // and update renderer / index HTML on file change.
     require(`./${BUILD_FOLDER}/setup-dev-server`)(app, {
         bundleUpdated: bundle => {
-            renderer = createRenderer(bundle);
+            renderer = vueJsServerRenderer(bundle);
         },
         indexUpdated: index => {
-            generatedHtml = parseIndex(index);
+            generatedHtml = findPlaceholderInTemplateAndReplace(index);
         }
     })
 }
 
-const serve = (path, cache) => express.static(resolve(path), {
-    maxAge: cache && IS_PROD ? 60 * 60 * 24 * 30 : 0
-});
+/**
+ * Gzip compression is a must
+ * */
+app.use(compression({
+    threshold: 0,
+    level: zLib.Z_BEST_COMPRESSION
+}));
 
-app.use(compression({ threshold: 0 }));
-app.use('/service-worker.js', serve(`./${DISTRIBUTION_FOLDER}/service-worker.js`));
-app.use(`/${DISTRIBUTION_FOLDER}`, serve(`./${DISTRIBUTION_FOLDER}`));
+/**
+ * Indicating our static folder and setting caching duration
+ * */
+app.use(`/${DISTRIBUTION_FOLDER}`, staticAsset(path.resolve(__dirname) + '/dist/', { maxAge: ONE_YEAR_IN_MILLIS }));
+app.use(`/${DISTRIBUTION_FOLDER}`, express.static(path.resolve(__dirname) + '/dist/', { maxAge: ONE_YEAR_IN_MILLIS }));
 
+/**
+ * Do following with all incoming GET requests
+ * */
 app.get('*', (req, res) => {
-    if (!renderer) {
-        return res.end('Waiting for compilation... refresh in a moment.');
-    }
-
-    res.setHeader('Content-Type', 'text/html');
-    res.setHeader('Server', SERVER_INFO);
-
-    const TIME_AT_START_REQUEST = Date.now();
+    if (!renderer) { return res.end('Waiting for compilation... refresh in a moment.'); }
     const context = { url: req.url };
     const renderStream = renderer.renderToStream(context);
+
+    res.setHeader('Content-Type', 'text/html');
 
     renderStream.once('data', () => {
         res.write(generatedHtml.head);
@@ -86,7 +90,6 @@ app.get('*', (req, res) => {
             res.write(`<script>window.__INITIAL_STATE__=${serialize(context.initialState, { isJSON: true })}</script>`);
         }
         res.end(generatedHtml.tail);
-        console.log(`whole request: ${Date.now() - TIME_AT_START_REQUEST}ms`);
     });
 
     renderStream.on('error', err => {
@@ -101,6 +104,9 @@ app.get('*', (req, res) => {
     });
 });
 
-app.listen(PORT, () => {
-    console.log(`Server started at http://localhost:${PORT}`);
+/**
+ * Run app at port
+ * */
+app.listen(APP_PORT_NUM, () => {
+    console.log(`Server started at http://localhost:${APP_PORT_NUM}`);
 });
