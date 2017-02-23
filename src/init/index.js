fd955c05cab3867dbdc605531c21829b997ba950
Arman Abbasi <arman.abbasi@marksandspencer.com>
2017-02-23 09:42:46 +0000

    loading babel config to babel register

--- src/init/index.js
+++ src/init/index.js
@@ -1,3 +1,7 @@
-require('babel-core/register')();
+let fs = require('fs');
+let path = require('path');
+let config = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../../.babelrc')));
+
+require('babel-core/register')(config);
 require('../../config/build/setup-dev-server');
 require('./server');
